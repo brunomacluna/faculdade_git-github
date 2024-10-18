4f36d0ffcd9c107d8571022c2ca030c0815f90a3
Bruno Machado Luna <165810990+brunomacluna@users.noreply.github.com>
2024-10-18 16:45:11 -0300

    add

--- 2º_Semestre/python_linguagem_tecnica/exercicios_de_python-orientacao_a_objetos/gerenciamento-de-alunos.py
+++ 2º_Semestre/python_linguagem_tecnica/exercicios_de_python-orientacao_a_objetos/gerenciamento-de-alunos.py
@@ -1,37 +1,32 @@
 class Aluno:
-    def __init__(self, nome, idade, nota):
+    def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
-        self.nota = nota
         self.notas = []
 
     def adicionar_nota(self,nota):
         self.notas.append(nota)
 
     def media_notas(self):
-        return sum(self.notas) / len(self.notas) 
-    
+        if len(self.notas) > 0:
+            return sum(self.notas) / len(self.notas) 
+        else:
+            return 0
+        
     def exibir_informacoes(self):
         media = self.media_notas()
-        print(f"Nome: {self.nome} Idade: {self.idade} Média: {media}")
+        print(f"Nome: {self.nome} Idade: {self.idade} Média: {media:.2}")
 
-obj_Aluno = []
-while True:
+lista_Alunos = []
+contador = 3 # coloquei o contador em 3 porque vou precisar de somente 3 alunos
+
+while contador > 0:
     nome = input("Digite o nome: ")
-    idade = int(input("Digite a idade: "))
-    contador = int(input("Quantas notas serão inseridas: "))
-    notas = []
-    
-
-    aluno = Aluno(nome, idade, notas)
-    obj_Aluno.append(aluno)
-
-    continuar = input("Deseja adicionar outro aluno? [S/N] ").upper()
-    if continuar == "N":
-        break
-
-for aluno in obj_Aluno:
-    aluno.exibir_informacoes()
+    indade = int(input("Digite a idade: "))
+    cont_notas = 1
+    while cont_notas <= 4:
+        nota = float(input(f"Digite {cont_notas}º nota: "))
+        cont_notas += 1
 
 
 
