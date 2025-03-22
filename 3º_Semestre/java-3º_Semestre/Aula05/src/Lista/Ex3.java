c2eaac7b466b6b86690f196b2fa8f1e8fc5d9c26
Bruno Machado Luna <165810990+brunomacluna@users.noreply.github.com>
2025-03-22 18:22:09 -0300

    classe Ex3 de java aula05

--- 3º_Semestre/java-3º_Semestre/Aula05/src/Lista/Ex3.java
+++ 3º_Semestre/java-3º_Semestre/Aula05/src/Lista/Ex3.java
@@ -7,14 +7,44 @@ public class Ex3 {
 		Scanner input = new Scanner(System.in);
 		
 		int positivos = 0;
-		int netagivos = 0;
+		int negativos = 0;
+		int contador = 0;
+		double media_aritimetica = 0;
+		int total = 0;
 		
 		while (true) {
 			System.out.print("Digite um valor: ");
+			int valor = input.nextInt();
+			contador++;
+			total += valor;
 			
-		}//fim while	
+			if (valor > 0) {
+				positivos++;
+			}//fim if
+			else {
+				negativos++;
+			}//fim if else
+			
+			if (positivos > 0 && negativos > 0) {
+				System.out.print("Deseja inserir mais números? [s/n]" );
+				if (input.next().toLowerCase().equals("n")) {
+					break;
+				}//fim if
+			} else {
+				System.out.println("Você precisa inserir pelo um número positivo e um negativo: ");
+			}//fim else
+		}//fim while 1	
+				
+		
+		media_aritimetica = (double) total / contador;
+		
+		System.out.println("Média aritimética: "+media_aritimetica);
+		System.out.println("Números positivos: "+positivos);
+		System.out.println("Números negativos: "+negativos);
+		System.out.println("Porcentagem dos positivos: "+((double)positivos / total * 100));
+		System.out.println("Porcentegam do negativos: "+((double)negativos / total * 100));
 	}//fim calculoEx3
-}//fim class
+}//fim class Ex3
 /*
 3. Crie um programa que solicite ao usuário uma quantidade indefinida de valores
 numéricos. O programa deve calcular e exibir a média aritmética dos números
