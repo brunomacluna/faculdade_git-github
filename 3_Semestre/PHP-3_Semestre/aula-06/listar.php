139f2c0adfb8280ddf448c4631bbcff6373165e0
Bruno Machado Luna <165810990+brunomacluna@users.noreply.github.com>
2025-04-15 21:21:06 -0300

    edições php

--- 3_Semestre/PHP-3_Semestre/aula-06/listar.php
+++ 3_Semestre/PHP-3_Semestre/aula-06/listar.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
 <?php
 
 include('conexao.php');
@@ -15,7 +17,7 @@ $res = $conn -> query($sql);
 
 $qtde = $res -> num_rows;
 if($qtde > 0){
-    print('<table>.');
+    print('<table class="table table-striped">');
     print('<tr>');
     print('<th> Código</th>');
     print('<th> Nome</th>');
@@ -23,20 +25,26 @@ if($qtde > 0){
     print('<th> Email</th>');
     print('<th> CPF</th>');
     print('<th> Data</th>');
+    print('<th> Ações</th>');
     print('</tr>');
 
     while($row = $res -> fetch_object()){
         print('<tr>');
-        print('<td>'.$row -> $cod.'</td>');
-        print('<td>'.$row -> $nome.'</td>');
-        print('<td>'.$row -> $senha.'</td>');
-        print('<td>'.$row -> $email.'</td>');
-        print('<td>'.$row -> $cpf.'</td>');
-        print('<td>'.$row -> $data.'</td>');
+        print('<td>'.$row -> codigo.'</td>');
+        print('<td>'.$row -> nome.'</td>');
+        print('<td>'.$row -> senha.'</td>');
+        print('<td>'.$row -> email.'</td>');
+        print('<td>'.$row -> cpf.'</td>');
+        print('<td>'.$row -> data.'</td>');
+        print('<td>
+        <button class="btn btn-success">Editar</button>
+        <button class="btn btn-danger">Excluir</button>
+        </td>');
+        
         print('</tr>');
-        print('</table>');
+        
 }
-
+print('</table>');
 }
 
 else{
@@ -45,6 +53,7 @@ else{
 
  ?>
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</div>
 </body>
 </html>
