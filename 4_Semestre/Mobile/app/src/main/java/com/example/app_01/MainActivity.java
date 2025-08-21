5cd44ca4ae35101ece647b8feb99f2c0aa6f04e1
Bruno Machado Luna <165810990+brunomacluna@users.noreply.github.com>
2025-08-21 14:58:33 -0300

    configurando meu celular para servir de teste

--- 4_Semestre/Mobile/app/src/main/java/com/example/app_01/MainActivity.java
+++ 4_Semestre/Mobile/app/src/main/java/com/example/app_01/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app_01;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -11,6 +12,15 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(context: this, )
+
+        // Exibe o Toast
+        Toast toast = Toast.makeText(this, "Hello World", Toast.LENGTH_LONG);
+        toast.show();
+
+        // Reexibe o Toast após 2 segundos para totalizar ~5 segundos
+        new Handler().postDelayed(() -> {
+            Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+        }, 2000);
     }
-}//class
+}
+
